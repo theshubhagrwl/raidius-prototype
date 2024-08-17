e9d443f4d6563bde8344134174336376941ff382
Shubh Agrawal <shubhagrawal4457@gmail.com>
2024-08-17 10:47:54 +0530

    updated socket link

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -2,7 +2,8 @@ import { io } from 'socket.io-client'
 
 const SERVER =
   process.env.NODE_ENV === 'production'
-    ? 'https://scribble-production-d6c0.up.railway.app'
+  // ? 'https://scribble-production-d6c0.up.railway.app'
+    ? 'https://raidius-prototype-be.vercel.app'
     : 'http://localhost:3001'
 
 export const socket = io(SERVER, { transports: ['websocket'] })
